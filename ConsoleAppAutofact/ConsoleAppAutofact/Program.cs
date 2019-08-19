179bac5f9397b5c8ec55b4ecaa5705b25e8c48eb
hermilocap <hermilocap@hotmail.com>
2019-08-18 19:54:47 -0500

    Ejercicio de autofac

---
+++ ConsoleAppAutofact/ConsoleAppAutofact/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace ConsoleAppAutofact
+{
+    class Program
+    {
+        private static IContainer container { get; set; }
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As <IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            container = builder.Build();
+            WriteDate();
+            Console.ReadKey();
+
+        }
+      public static void WriteDate()
+        {
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+        }
+    }
+}
---
+++ ConsoleAppAutofact/ConsoleAppAutofact/TodayWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAutofact
+{
+    class TodayWriter:IDateWriter
+    {
+        private IOutput _output;
+        public TodayWriter(IOutput output)
+        {
+            _output = output;
+        }
+       
+        public void WriteDate()
+        {
+            _output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+}
